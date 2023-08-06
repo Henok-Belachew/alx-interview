cc8214a4cbdbaeb159f0496d4fb39106abb0a7c6
Henok Belachew <henokbelachewstu@gmail.com>
2023-08-06 22:37:39 +0300

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,66 +1,54 @@
 #!/usr/bin/python3
 """
-NQueens Solver
+N queens
 """
+
 import sys
 
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
 
-def checksValid(board, row, col):
-    """Checks if position is valid
-    """
-    b_size = len(board)
-    if sum(board[row]) or sum([board[i][col] for i in range(b_size)]) != 0:
-        return False
+try:
+    n_q = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
 
-    for i, j in [(1, 1), (-1, -1), (1, -1), (-1, 1)]:
-        r, c = row, col
-        while 0 <= r + i < b_size and 0 <= c + j < b_size:
-            r, c = r + i, c + j
-            if board[r][c]:
-                return False
+if n_q < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+
+def attack_queen(square, queen):
+    '''self descriptive'''
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
+            return False
     return True
 
 
-def solution_queens(board, row, solutions):
-    """Solves the nqueens problem
-    :param board: board
-    :param row: row
-    :param solutions: solutions
-    """
-    n = len(board)
-    if row == n:
-        solutions.append([[r, board[r].index(1)] for r in range(n)])
-        return
-
-    for col in range(n):
-        if checksValid(board, row, col):
-            board[row][col] = 1
-            solution_queens(board, row + 1, solutions)
-            board[row][col] = 0
-
-
-def print_sol(n):
-    """Prints the solutions
-    """
-    board = [[0 for col in range(n)] for row in range(n)]
-    solutions = []
-    solution_queens(board, 0, solutions)
-    for row in solutions:
-        print(row)
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-        if n < 4:
-            print('N must be at least 4')
-            sys.exit(1)
-        print_sol(n)
-
-    except ValueError:
-        print('N must be a number')
-        sys.exit(1)
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
